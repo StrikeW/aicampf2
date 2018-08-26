c1aafbe3f125cd1126236405c952b0daa223ba91
StrikeW <wangsiyuanse@gmail.com>
2018-08-26 11:10:22 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'flask',
+    'requests>=2.17.3',
+    'uuid',
+    'gunicorn',
+    'Flask',
+    'numpy',
+    'pandas',
+    'python-dateutil',
+    'querystring_parser',
+]
+
+setup(
+    name='flask_todo',
+    version='0.0',
+    description='A Simple Machine Learning Workflow',
+    author='F2',
+    author_email='<Your actual e-mail address here>',
+    keywords='web flask',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires
+)
