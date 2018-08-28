de06ec97cab64be314c20ba0422a7291b45c1c16
StrikeW <wangsiyuanse@gmail.com>
2018-08-28 10:52:31 +0800

    minor

--- server/config.py
+++ server/config.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-__all__ = ['model_saved_path', 'project_root']
+__all__ = ['upload_path', 'project_root']
 
-model_saved_path = 'saved_model'
-project_root = 'C:\\Users\\admin\\aicampf2\\aicampf2'
-upload_path = 'C:\\Users\\admin\\aicampf2\\uploads'
+project_root = '/Users/siyuan/workspace/AICamp/aicampf2'
+upload_path = '/Users/siyuan/workspace/AICamp/aicampf2/uploads'
