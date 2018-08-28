b2a4b4489eeb3fa31f07a49cb812bd8c6f081f2f
qwer <oopsvpwowlq@gmail.com>
2018-08-28 13:53:14 +0800

    修改左侧栏

--- server/config.py
+++ server/config.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 
-__all__ = ['upload_path', 'project_root']
+__all__ = ['model_saved_path', 'project_root']
 
-project_root = 'C:\\Users\\admin\\aicampf2'
-upload_path = 'C:\\Users\\admin\\aicampf2\\uploads'
+model_saved_path = 'saved_model'
+project_root = 'C:\\Users\\admin\\aicampf2\\aicampf2'
+upload_path = 'C:\\Users\\admin\\aicampf2\\aicampf2\\uploads'
